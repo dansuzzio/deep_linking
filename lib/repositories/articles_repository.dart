3982dd287909dd11e6b2250b87f81c0b0fa798ac
Daniel Suzzio <dansuzzio@gmail.com>
2023-04-30 10:13:57 -0300

    update articles repository results

--- lib/repositories/articles_repository.dart
+++ lib/repositories/articles_repository.dart
@@ -2,16 +2,23 @@ import 'package:faker/faker.dart';
 
 import '../models/article.dart';
 
-abstract class ArticlesRepository {
-  static final _articles = List.generate(
-    10,
-    (index) => Article(
-      topic: 'Animals',
-      title: faker.animal.name(),
+final _animals = ['Turtle', 'Koala', 'Whale', 'Panda', 'Tiger', 'Zebra', 'Penguin'];
+final _books = ['The Great Gatsby', 'The Grapes of Wrath', 'Nineteen Eighty-Four', 'Ulysses', 'Lolita', 'Catch-22', 'Beloved'];
+final _food = ['Pizza', 'Burger', 'Pasta', 'Salad', 'Soup', 'Sandwich', 'Steak'];
+
+Article _getArticle(String topic, String title) => Article(
+      topic: topic,
+      title: title,
       date: faker.date.dateTime(minYear: 2010, maxYear: 2022),
       content: faker.lorem.sentences(10).join(' '),
-    ),
-  );
+    );
+
+abstract class ArticlesRepository {
+  static final _articles = [
+    ...List.generate(_animals.length, (index) => _getArticle('Animals', _animals[index])),
+    ...List.generate(_books.length, (index) => _getArticle('Books', _books[index])),
+    ...List.generate(_food.length, (index) => _getArticle('Food', _food[index])),
+  ];
 
   static List<Article> get articles => _articles;
 
